da7ef50b6312102749f3a3817e3eda60fd10cdff
jyzxc <867137498@qq.com>
2024-10-25 17:41:03 +0800

    feat: add the menu panel

---
+++ app/src/main/java/com/caodong0225/flappybird/view/MapScreen.kt
@@ -0,0 +1,13 @@
+package com.caodong0225.flappybird.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+class MapScreen : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
